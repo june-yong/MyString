ca2652cb46084de27985118ea889463a1955f2bc
june-yong <junyong077@gmail.com>
2024-02-29 20:09:58 +0900

    여기까지 ....

--- StringMain.cpp
+++ StringMain.cpp
@@ -5,9 +5,11 @@ int main()
 {
     MyString str1 = "Hellow World !!";
     MyString str2 = "World";
+    MyString str3("오늘은");
 
     std::cout << str1 << std::endl;
     std::cout << str2 << std::endl;
+    std::cout << str3 << std::endl;
 
     std::cout << str1 + str2 << std::endl;
     std::cout << str1 + " " + str2 << std::endl;
